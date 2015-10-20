f3c64149e87dd02483ed4abef7a3a8e07903afe8
julitosnchez <julitosnchez@julitosnchez-nigga>
2015-10-20 17:57:21 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ NapakalakiR/lib/main.PruebaNapakalakiR.rb
@@ -0,0 +1,7 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
+m = Prize.new(1,2)
+puts m.treasures
