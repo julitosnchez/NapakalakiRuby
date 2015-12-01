89b2a47ca0bfca42a3f9ac484554023f44751721
julitosnchez <julitosnchez@julitosnchez-nigga>
2015-12-01 20:55:24 +0100

    Merge origin/master
    Conflicts:
    	src/napakalaki/Napakalaki.java

--- lib/bad_consequence.rb
+++ lib/bad_consequence.rb
@@ -54,7 +54,7 @@ module Napakalaki
     end
 
     def is_empty()
-      if @levels == 0 && @n_visible_treasures == 0 && @n_hidden_treasures == 0 && death == false && @specific_hidden_treasures == [] && @specific_visible_treasures == []
+      if @levels == 0 && @n_visible_treasures == 0 && @n_hidden_treasures == 0 && @death == false && @specific_hidden_treasures == [] && @specific_visible_treasures == []
         return true
       end
       return false
--- lib/player.rb
+++ lib/player.rb
@@ -76,8 +76,8 @@ class Player
   
   def discard_visible_treasure(t)
     @visible_treasures.delete(t)
-    if(@pending_bad_consequence.nil? != false)
-      if(@pending_bad_consequence.empty? != false)
+    if(@pending_bad_consequence.nil? == false)
+      if(@pending_bad_consequence.empty? == false)
         @pending_bad_consequence.substract_visible_treasures(t)
       end
     end
@@ -86,8 +86,8 @@ class Player
   
   def discard_hidden_treasure(t)
      @hidden_treasures.delete(t)
-    if(@pending_bad_consequence.nil? != false)
-      if(@pending_bad_consequence.empty? != false)
+    if(@pending_bad_consequence.nil? == false)
+      if(@pending_bad_consequence.empty? == false)
       @pending_bad_consequence.substract_hidden_treasure(t)
       end
     end
@@ -192,7 +192,7 @@ class Player
   end
   
   def increment_levels(l)
-    if @level+l < @@MAX_LEVEL
+    if @level+l <= @@MAX_LEVEL
       @level = @level+l
     else
        @level = @@MAX_LEVEL
@@ -200,7 +200,7 @@ class Player
   end
   
   def decrement_levels(l)
-    if @level-l > 1
+    if @level-l >= 1
       @level = @level -l
     else
       @level = 1
